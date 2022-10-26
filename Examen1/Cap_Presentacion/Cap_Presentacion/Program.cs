8cf0ca612ce27870c90cc3a3f341b21a6797ca45
Joss-Bar <106713950+Joss-Bar@users.noreply.github.com>
2022-10-25 22:34:03 -0600

    cambios en la capa de presentacion
    Modificacion de paramentros

--- Examen1/Cap_Presentacion/Cap_Presentacion/Program.cs
+++ Examen1/Cap_Presentacion/Cap_Presentacion/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Cap_Presentacion
+namespace Cap_Presentacion // agregar detalle
 {
     internal static class Program
     {
